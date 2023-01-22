11ee08deffa95219b016ea750d8692d003d76869
atanda nafiu <atandanafiu21@gmail.com>
2023-01-22 03:01:49 +0100

    commit

--- main.go
+++ main.go
@@ -25,12 +25,32 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 
-	r.Handlefunc("/movies", getMovies).Methods("GET")
-	r.Handlefunc("/movies/{id}", getMovies).Methods("Get")
-	r.Handlefunc("/movies", createMovie).Methods("POST")
-	r.Handlefunc("/movies/{id}", updateMovie).Methods("PUT")
-	r.Handlefunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "438227",
+		Title: "Movie one",
+		Director: &Director{
+			FirstName: "John",
+			LastNam:   "Doe",
+		},
+	})
 
-	fmt.Printf("Starting server at port 8080")
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "45455",
+		Title: "Movie two",
+		Director: &Director{
+			FirstName: "Atanda",
+			LastNam:   "0x",
+		},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("Get")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
